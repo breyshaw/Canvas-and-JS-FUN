b7f7e169475997c63afeb0c77e1bae85aa288d91
breyshaw <dawsonbrey@gmail.com>
2022-01-21 17:07:25 -0500

    add gravity functionality

--- js/app.js
+++ js/app.js
@@ -6,11 +6,18 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
+const gravity = 0.5
+
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100
+    }
+    this.velocity = {
+      x: 0,
+      //y velocity pushes the player down
+      y: 0
     }
     this.width = 30
     this.height = 30
@@ -20,8 +27,27 @@ class Player {
     c.fillStyle = 'blue'
     c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
+
+  update() {
+    this.draw()
+    this.position.y += this.velocity.y
+    //This function monitors the bottom of the player, and if the bottom of our player plus its velocity is less than the bottom of the canvas, gravity continues. But when bottom of the player + velocity is = canvas.height, it sets the players y velocity to 0.
+    if (this.position.y + this.height + this.velocity.y <= canvas.height)
+    this.velocity.y += gravity
+    else this.velocity.y = 0
+  }
 }
 
 const player = new Player()
-//This basically invokes the player
+
 player.draw()
+
+//recursive animation loop
+function animate() {
+  requestAnimationFrame(animate)
+  //this clears the cavas
+  c.clearRect(0 , 0, canvas.width, canvas.height)
+  player.update()
+}
+
+animate()
