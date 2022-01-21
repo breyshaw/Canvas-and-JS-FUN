3f03cbea4bc3d94335e6c23b2b586de7a1cd8501
breyshaw <dawsonbrey@gmail.com>
2022-01-21 16:43:11 -0500

    adjust player size and color

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector('canvas')
 
 const c = canvas.getContext('2d')
 
+//Need the canvas to fit the entire screen, can also be done in a stylesheet
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
@@ -11,14 +12,16 @@ class Player {
       x: 100,
       y: 100
     }
-    this.width = 100
-    this.height = 100
+    this.width = 30
+    this.height = 30
   }
-
+//This is the method that "draws" or creates the character on the canvas
   draw() {
+    c.fillStyle = 'blue'
     c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 }
 
 const player = new Player()
+//This basically invokes the player
 player.draw()
