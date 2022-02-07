e03b7cab8678d052cf505fac9ee42be3ac43c409
breyshaw <dawsonbrey@gmail.com>
2022-02-07 15:48:58 -0500

    alter platform constructor method to remove static positions, create multiple platforms

--- js/app.js
+++ js/app.js
@@ -40,10 +40,10 @@ class Player {
 }
 
 class Platform {
-  constructor(){
+  constructor({x,y}){
     this.position = {
-      x: 200,
-      y: 550,
+      x: x,
+      y: y,
     }
     this.width = 200
     this.height = 20
@@ -55,7 +55,7 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
+const platforms = [new Platform({x: 100, y:600}), new Platform({x: 450, y: 400})]
 const keys = {
   right: {
     pressed: false
@@ -72,18 +72,35 @@ function animate() {
   //this clears the canvas
   c.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
-  platform.draw()
+  //This draws multiple platforms
+  platforms.forEach(platform => {
+    platform.draw()
+  })
 
-  if (keys.right.pressed) {
+  if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5
-  } else if (keys.left.pressed) {
+  } else if (keys.left.pressed && player.position.x > 100 ) {
     player.velocity.x = -5
-  } else player.velocity.x = 0
-  
+  } else {
+    player.velocity.x = 0
+
+    if (keys.right.pressed) {
+      platforms.forEach(platform => {
+        platform.position.x -= 5
+      })
+    } else if (keys.left.pressed) {
+      platforms.forEach(platform => {
+        platform.position.x += 5
+      })
+    }
+  }
+
 //Platform collision detection
+platforms.forEach(platform => {
   if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
     player.velocity.y = 0
   }
+})
 }
 
 animate()
