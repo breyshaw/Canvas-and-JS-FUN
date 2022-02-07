8ddd64afde18ce434ea1a7f5d2515a86c2aa83df
breyshaw <dawsonbrey@gmail.com>
2022-02-07 15:14:46 -0500

    add even listeners for keyup and keydown, finish x and y player movement

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ class Player {
     this.width = 30
     this.height = 30
   }
-//This is the method that "draws" or creates the character on the canvas
+  //This is the method that "draws" or creates the character on the canvas
   draw() {
     c.fillStyle = 'blue'
     c.fillRect(this.position.x, this.position.y, this.width, this.height)
@@ -30,24 +30,54 @@ class Player {
 
   update() {
     this.draw()
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
     //This function monitors the bottom of the player, and if the bottom of our player plus its velocity is less than the bottom of the canvas, gravity continues. But when bottom of the player + velocity is = canvas.height, it sets the players y velocity to 0.
     if (this.position.y + this.height + this.velocity.y <= canvas.height)
-    this.velocity.y += gravity
+      this.velocity.y += gravity
     else this.velocity.y = 0
   }
 }
 
-const player = new Player()
+class Platform {
+  constructor(){
+    this.position = {
+      x: 0,
+      y: 0,
+    }
+    this.width = 200
+    this.height = 20
+  }
+  draw() {
+    c.fillStyle = 'green'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
 
+const player = new Player()
+const keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+}
 player.draw()
 
 //recursive animation loop
 function animate() {
   requestAnimationFrame(animate)
   //this clears the canvas
-  c.clearRect(0 , 0, canvas.width, canvas.height)
+  c.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
+
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5
+  } else player.velocity.x = 0
 }
 
 animate()
@@ -68,20 +98,40 @@ animate()
 // })
 
 
-addEventListener('keydown', ({keyCode}) => {
+addEventListener('keydown', ({ keyCode }) => {
   console.log(keyCode)
   switch (keyCode) {
     case 65:
-      console.log('left')
+      keys.left.pressed = true
       break
-      case 83:
+    case 83:
       console.log('down')
       break
-      case 68:
-      console.log('right')
+    case 68:
+      keys.right.pressed = true
       break
-      case 87:
-      console.log('up')
+    case 87:
+      player.velocity.y -= 20
       break
   }
+  console.log(keys.right.pressed)
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+  console.log(keyCode)
+  switch (keyCode) {
+    case 65:
+      keys.left.pressed = false
+      break
+    case 83:
+      console.log('down')
+      break
+    case 68:
+      keys.right.pressed = false
+      break
+    case 87:
+      player.velocity.y -= 20
+      break
+  }
+  console.log(keys.right.pressed)
 })
